ab84a0ca10a648620882b9a85ccaaf655b79977b
Laurent Meunier <laurent@deltalima.net>
2014-11-29 11:57:06 +0100

    Add a command flag to display Gorgon version number

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"flag"
 	"github.com/lmeunier/gorgon/app"
+	"os"
 )
 
 func main() {
 	config_file := flag.String("c", "gorgon.ini", "Path to the Gorgon configuration file.")
+	version := flag.Bool("v", false, "Prints the Gorgon version and exits.")
 	flag.Parse()
+
+	if *version {
+		// prints the Gorgon version and exits
+		print("Gorgon v" + app.Version + "\n")
+		os.Exit(1)
+	}
 
 	gorgon_app := app.NewApp(*config_file)
 	gorgon_app.Logger.Info("Starting Gorgon v" + app.Version + " (config: " + *config_file + ")")
